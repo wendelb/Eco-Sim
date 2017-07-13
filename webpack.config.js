388505c1a988efc3dbcafc10778442fc3a03ef57
Bernhard Wendel <bwendel@gmx.de>
2017-07-13 22:10:03 +0000

    First Step to Webpack-Solution: Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    //new CleanWebpackPlugin(['dist'])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
