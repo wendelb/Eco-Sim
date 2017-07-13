ad0742c5c3a097a02113de02c231c78752d2b688
Bernhard Wendel <bwendel@gmx.de>
2017-07-13 22:24:23 +0000

    Make Webpack output something

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -8,7 +9,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    //new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: './src/index.html' },
+      { from: './src/app.css' }
+    ])
   ],
   module: {
     rules: [
